619fb14c0b45b114f76b7efccae6bec52f952c4d
Justin Bailey <jbailey@asheworks.com>
2020-11-10 11:52:01 -0800

    Test case for random graphql calls.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.route2("POST", "/graphql", {
+      statusCode: 200,
+      body: 'it worked!'
+    }).as("graphql")
+    cy.visit('cypress/integration/index.html')
+    cy.wait("@graphql", { timeout: 10000 }).then(({ request }) =>  {
+      console.log(`${JSON.stringify(request)}`)
+      const { operationName } = JSON.parse(request.body)
+      expect(operationName).to.equal("op2")
+    })
   })
 })
